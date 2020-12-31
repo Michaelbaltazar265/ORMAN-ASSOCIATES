83b88beb32439ffd96bf69b4e0b47c7c845d0ddb
Michaelbaltaza265 <michaelbaltazar265@gmail.com>
2020-12-31 00:22:29 +0000

    having the menu stay in one position

---
+++ orman-associates/src/compontents/ContactUs.jsx
@@ -0,0 +1,14 @@
+import React from 'react'; 
+
+const ContactUs = () => { 
+
+    return ( 
+    <section className='contact-us' id='contact-us'>
+        <div className='container text-center'>
+            <h1>Contact Us</h1>
+        </div>
+    </section>
+    )
+}
+
+export default ContactUs;
